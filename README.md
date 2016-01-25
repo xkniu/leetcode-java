7a99ee6d0b869cbe0cb31f0060f98474f291f5f2
Xuekai Niu <xuekai.niu@gmail.com>
2016-01-25 18:03:08 +0800

    Solution of problem 2: Add Two Numbers

--- README.md
+++ README.md
@@ -6,4 +6,5 @@ Solutions for questions on [LeetCode Online Judge](https://leetcode.com/).
 
 | # | Title | Solution | Difficulty |
 |---| ----- | -------- | ---------- |
+| 2 | [Add Two Numbers](https://leetcode.com/problems/add-two-numbers/) | [Source](./src/main/java/AddTwoNumbers.java) | Medium |
 | 1 | [Two Sum](https://leetcode.com/problems/two-sum/) | [Source](./src/main/java/TwoSum.java) | Medium |
---
+++ src/main/java/AddTwoNumbers.java
@@ -0,0 +1,34 @@
+public class AddTwoNumbers {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode pre = dummy;
+        int unitCarry = 0;
+        while (l1 != null || l2 != null || unitCarry != 0) {
+            int unitSum = unitCarry;
+            if (l1 != null) {
+                unitSum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                unitSum += l2.val;
+                l2 = l2.next;
+            }
+            unitCarry = unitSum / 10;
+            pre.next = new ListNode(unitSum % 10);
+            pre = pre.next;
+        }
+        return dummy.next;
+    }
+
+    private static class ListNode {
+
+        int val;
+
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
