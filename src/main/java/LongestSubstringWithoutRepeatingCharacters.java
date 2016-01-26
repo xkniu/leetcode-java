16ce026fca6bb29f6c74329d5927741e54648d26
Xuekai Niu <xuekai.niu@gmail.com>
2016-01-26 19:48:11 +0800

    Solution of problem 3: Longest Substring Without Repeating Characters

---
+++ src/main/java/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+
+    public int lengthOfLongestSubstring(String s) {
+        int max = 0;
+        Map<Character, Integer> indexByChar = new HashMap<>();
+        int start = 0;
+        // [start + max >= i] is always true, means the remainders are enough to get [max + 1]
+        for (int i = 0; start + max < s.length(); ++i) {
+            Integer preIndex = indexByChar.get(s.charAt(i));
+            if (preIndex != null && preIndex >= start) {
+                start = preIndex + 1;
+            } else {
+                max = Math.max(i - start + 1, max);
+            }
+            indexByChar.put(s.charAt(i), i);
+        }
+        return max;
+    }
+
+    // When data size is small, using "indexOf" may be better than hashmap
+    public int lengthOfLongestSubstring2(String s) {
+        int max = 0;
+        int start = 0;
+        // [start + max >= i] is always true, means the remainders are enough to get [max + 1]
+        for (int i = 0; start + max < s.length(); ++i) {
+            int preIndex = s.indexOf(s.charAt(i), start);
+            if (preIndex != i) {
+                start = preIndex + 1;
+            } else {
+                max = Math.max(i - start + 1, max);
+            }
+        }
+        return max;
+    }
+}
