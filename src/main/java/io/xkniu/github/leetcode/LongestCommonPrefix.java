e13f0a28d2b92bb875dbde28c18cd3b8b5ef4444
Xuekai Niu <xuekai.niu@gmail.com>
2018-12-28 00:38:30 +0800

    Problem 14

---
+++ src/main/java/io/xkniu/github/leetcode/LongestCommonPrefix.java
@@ -0,0 +1,22 @@
+package io.xkniu.github.leetcode;
+
+/**
+ * #14, https://leetcode.com/problems/longest-common-prefix/
+ */
+public class LongestCommonPrefix {
+
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+
+        for (int i = 0; i < strs[0].length(); i++) {
+            for (int j = 1; j < strs.length; j++) {
+                if (i == strs[j].length() || strs[0].charAt(i) != strs[j].charAt(i)) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        return strs[0];
+    }
+}
--- src/test/java/io/xkniu/github/leetcode/LeetcodeTest.java
+++ src/test/java/io/xkniu/github/leetcode/LeetcodeTest.java
@@ -7,13 +7,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class LeetcodeTest {
 
     @Test
-    public void test1() throws Exception {
+    public void test1() {
         TwoSum solution = new TwoSum();
-        assertThat(solution.twoSum(new int[] {2, 7, 11, 15}, 9)).isEqualTo(new int[] {0, 1});
+        assertThat(solution.twoSum(new int[]{2, 7, 11, 15}, 9)).isEqualTo(new int[]{0, 1});
     }
 
     @Test
-    public void test7() throws Exception {
+    public void test7() {
         ReverseInteger solution = new ReverseInteger();
         assertThat(solution.reverse(123)).isEqualTo(321);
         assertThat(solution.reverse(-123)).isEqualTo(-321);
@@ -25,7 +25,7 @@ public class LeetcodeTest {
     }
 
     @Test
-    public void test9() throws Exception {
+    public void test9() {
         PalindromeNumber solution = new PalindromeNumber();
         assertThat(solution.isPalindrome(121)).isEqualTo(true);
         assertThat(solution.isPalindrome(-121)).isEqualTo(false);
@@ -37,12 +37,19 @@ public class LeetcodeTest {
     }
 
     @Test
-    public void test13() throws Exception {
+    public void test13() {
         RomanToInteger solution = new RomanToInteger();
         assertThat(solution.romanToInt("III")).isEqualTo(3);
         assertThat(solution.romanToInt("IV")).isEqualTo(4);
         assertThat(solution.romanToInt("IX")).isEqualTo(9);
         assertThat(solution.romanToInt("LVIII")).isEqualTo(58);
         assertThat(solution.romanToInt("MCMXCIV")).isEqualTo(1994);
+    }
+
+    @Test
+    public void test14() {
+        LongestCommonPrefix solution = new LongestCommonPrefix();
+        assertThat(solution.longestCommonPrefix(new String[]{"flower", "flow", "flight"})).isEqualTo("fl");
+        assertThat(solution.longestCommonPrefix(new String[]{"dog", "racecar", "car"})).isEqualTo("");
     }
 }
